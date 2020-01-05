74ff954a98f8afc1be70afad4ec6be1713c78e49
Ruurd Bijlsma <ruurd@bijlsma.dev>
2020-01-05 22:55:38 +0100

    Change default ip

--- src/components/ServerBrowser.vue
+++ src/components/ServerBrowser.vue
@@ -89,7 +89,7 @@
         props: ['room', 'server'],
         data() {
             return {
-                serverIp: 'https://ruurd.dev:3000',
+                serverIp: 'https://api.ruurd.dev:3000',
                 roomName: '',
                 connectedServer: new ServerInfo('-1'),
                 serverUserCountUpdater: false,
